d9ba996d92880a34c61c989c3e969f8fd7b7507e
Jakub <jakub.solecki@onet.com.pl>
2020-03-23 13:52:50 +0100

    finished tasks

--- lab2/src/qsort.erl
+++ lab2/src/qsort.erl
@@ -10,7 +10,7 @@
 -author("Jakub").
 
 %% API
--export([qs/1, lessThan/2, grtEqThan/2, randomElems/3, compareSpeeds/3, map/2, filter/2, sumDigits/1]).
+-export([qs/1, lessThan/2, grtEqThan/2, randomElems/3, compareSpeeds/3]).
 
 
 % Quicksort
@@ -27,21 +27,3 @@ compareSpeeds(List, Fun1, Fun2) ->
   {T1, _} = timer:tc(Fun1, [List]),
   {T2, _} = timer:tc(Fun2, [List]),
   io:format("Sort1: ~p microseconds~nSort2: ~p microseconds~n", [T1, T2]).
-
-
-%% Fun
-map(_, []) -> [];
-map(F, [H | T]) -> [F(H) | qsort:map(F, T)].
-
-filter(_, []) -> [];
-filter(P, L) -> [Y || Y <- L, P(Y)].
-
-sumDigits(X) ->
-  L = [list_to_integer([Char]) || Char <- integer_to_list(X)],
-  lists:foldl(fun(Y, Acc) -> Y + Acc end, 0, L).
-
-
-%% From numbers in range [1, 1 000 000] pick those, which sum if digits is divisible by 3.
-%% (The two lines below ought to be run in Eshell in given order)
-%% L = qsort:randomElems(1000000, 1, 1000000).
-%% lists:filter(fun(X) -> case qsort:sumDigits(X) rem 3 == 0 of true -> true; (_) -> false end end, L).
