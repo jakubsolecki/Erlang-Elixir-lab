705680e09314dd2464210b9a363c34be6448d0af
Jakub <jakub.solecki@onet.com.pl>
2020-03-23 12:38:41 +0100

    finished

--- lab2/src/qsort.erl
+++ lab2/src/qsort.erl
@@ -10,9 +10,10 @@
 -author("Jakub").
 
 %% API
--export([qs/1, lessThan/2, grtEqThan/2, randomElems/3, compareSpeeds/3]).
+-export([qs/1, lessThan/2, grtEqThan/2, randomElems/3, compareSpeeds/3, map/2, filter/2, sumDigits/1]).
 
 
+% Quicksort
 qs([]) -> [];
 qs([Pivot | Tail]) -> qs(lessThan(Tail, Pivot)) ++ [Pivot] ++ qs(grtEqThan(Tail, Pivot)).
 
@@ -20,9 +21,7 @@ lessThan(List, Arg) -> [X || X <- List, X < Arg].
 
 grtEqThan(List, Arg) -> [X || X <- List, X >= Arg].
 
-%% Performance comparison
-
-randomElems(N, Min, Max) -> [rand:uniform(Max - Min + 1) + Min || _ <- lists:seq(1,  N)].
+randomElems(N, Min, Max) -> [rand:uniform(Max - Min + 1) + Min - 1 || _ <- lists:seq(1,  N)].
 
 compareSpeeds(List, Fun1, Fun2) ->
   {T1, _} = timer:tc(Fun1, [List]),
@@ -30,4 +29,19 @@ compareSpeeds(List, Fun1, Fun2) ->
   io:format("Sort1: ~p microseconds~nSort2: ~p microseconds~n", [T1, T2]).
 
 
+%% Fun
+map(_, []) -> [];
+map(F, [H | T]) -> [F(H) | qsort:map(F, T)].
 
+filter(_, []) -> [];
+filter(P, L) -> [Y || Y <- L, P(Y)].
+
+sumDigits(X) ->
+  L = [list_to_integer([Char]) || Char <- integer_to_list(X)],
+  lists:foldl(fun(Y, Acc) -> Y + Acc end, 0, L).
+
+
+%% From numbers in range [1, 1 000 000] pick those, which sum if digits is divisible by 3.
+%% (The two lines below ought to be run in Eshell in given order)
+%% L = qsort:randomElems(1000000, 1, 1000000).
+%% lists:filter(fun(X) -> case qsort:sumDigits(X) rem 3 == 0 of true -> true; (_) -> false end end, L).
