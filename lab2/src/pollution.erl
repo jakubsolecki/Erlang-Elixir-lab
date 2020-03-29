6bfe8837195d40e33eedf52a918761e650daab25
Jakub <jakub.solecki@onet.com.pl>
2020-03-29 15:51:18 +0200

    flattened date time and finished getOneValue/4

--- lab2/src/pollution.erl
+++ lab2/src/pollution.erl
@@ -10,115 +10,68 @@
 -author("Mithrian812").
 
 %% API
--export([createMonitor/0, addStation/3, addValues/5, removeValue/4]).
+-export([createMonitor/0, addStation/3, addValues/5, removeValue/4, getOneValue/4]).
 
 
-%%getOneValue(Station, {{Year, Month, Day}, Hour}, Type, [Stations, Readings]) ->
-%%  case is_tuple(Station) of
-%%    true -> getOneValue(Type, {{Year, Month, Day}, Hour}, lists:keyfind(Station, 2, Stations), [Stations, Readings]);
-%%    false -> getOneValue(Type, {{Year, Month, Day}, Hour}, lists:keyfind(Station, 1, Stations), [Stations, Readings])
-%%  end;
-%%getOneValue(_, _, false, [_, _]) ->
-%%  io:format("This station is not registered"),
-%%  noStationError;
-%%getOneValue(Type, {{Year, Month, Day}, Hour}, {Name, {Long, Lat}}, [Stations, Readings]) ->
-%%  case [{N, {{Y, M, D}, {H, M, S}}, T, V} || {N, {{Y, M, D}, {H, M, S}}, T, V} <- Readings,
-%%    {N, {{Y, M, D}, H}, T} == {Name, {{Year, Month, Day}, Hour}, Type}] of
-%%          [Success] -> Success;
-%%          [] -> case [{{L, T}, {{Y, M, D}, {H, M, S}}, T, V} || {{L, T}, {{Y, M, D}, {H, M, S}}, T, V} <- Readings,
-%%            {{L, T}, {{Y, M, D}, H}, T} == {Name, {{Year, Month, Day}, Hour}, Type}] of
-%%                  [Success] -> Success;
-%%                  [] -> io:format("Nothing was found"), noReadingError
-%%                end
-%%  end.
+createMonitor() -> [maps:new(), dict:new()].
 
 
-%% with dict and maps
-
-createMonitor() -> [maps:new(), maps:new(), dict:new()].
-
-
-%%TODO: one map for stations
-addStation(Name, {Long, Lat}, [StationNames, StationCoords, Readings]) ->
-  case {maps:find(Name, StationNames), maps:find({Long, Lat}, StationCoords)} of
-    {error, error} -> [maps:put(Name, {Name, {Long, Lat}}, StationNames), maps:put({Long, Lat},
-      {Name, {Long, Lat}}, StationCoords), Readings];
+addStation(Name, {Long, Lat}, [Stations, Readings]) ->
+  case {maps:find(Name, Stations), maps:find({Long, Lat}, Stations)} of
+    {error, error} ->
+      [maps:put(Name, {Name, {Long, Lat}}, maps:put({Long, Lat}, {Name, {Long, Lat}}, Stations)), Readings];
     _ -> io:format("This station is already registered!~n"), error
   end.
 
-
-addValues(Station, {{Year, Month, Day}, {Hour, Minute, Second}}, Type, Value, [StationNames, StationCoords, Readings]) ->
-  case {maps:find(Station, StationNames), maps:find(Station, StationCoords)} of
-    {error, error} -> io:format("This station is not registered yet!~n"), error;
-    {{ok, {Name, Coords}}, error} ->
-      checkBeforeAdd(Name, Coords, Station, {{Year, Month, Day}, {Hour, Minute, Second}}, Type, Value,
-        [StationNames, StationCoords, Readings]);
-    {error, {ok, {Name, Coords}}} ->
-      checkBeforeAdd(Name, Coords, Station, {{Year, Month, Day}, {Hour, Minute, Second}}, Type, Value,
-        [StationNames, StationCoords, Readings])
-  end.
-
-checkBeforeAdd(Name, Coords, Station, {{Year, Month, Day}, {Hour, Minute, Second}}, Type, Value,
-    [StationNames, StationCoords, Readings]) ->
-  case dict:is_key(Station, Readings) of
-    true ->
-     addNewValue(Station, {{Year, Month, Day}, {Hour, Minute, Second}}, Type, Value, dict:fetch(Station, Readings),
-       [StationNames, StationCoords, Readings]);
-    false ->
-      case dict:is_key(Name, Readings) of
+addValues(Station, {{Year, Month, Day}, {Hour, _, _}}, Type, Value, [Stations, Readings]) ->
+  case maps:find(Station, Stations) of
+    error -> io:format("This station is not registered yet!~n"), error;
+    {ok, StationKey} ->
+      case dict:is_key(StationKey, Readings) of
         true ->
-          addNewValue(Station, {{Year, Month, Day}, {Hour, Minute, Second}}, Type, Value, dict:fetch(Name, Readings),
-            [StationNames, StationCoords, Readings]);
+          addNewValue(StationKey, {{Year, Month, Day}, Hour}, Type, Value,
+            dict:fetch(StationKey, Readings), [Stations, Readings]);
         false ->
-          case dict:is_key(Coords, Readings) of
-            true ->
-              addNewValue(Station, {{Year, Month, Day}, {Hour, Minute, Second}}, Type, Value,
-                dict:fetch(Coords, Readings), [StationNames, StationCoords, Readings]);
-            false ->
-              [StationNames, StationCoords,
-                dict:append(Station, {{{Year, Month, Day}, {Hour, Minute, Second}}, Type, Value}, Readings)]
-          end
+          [Stations, dict:append(StationKey, {{{Year, Month, Day}, Hour}, Type, Value}, Readings)]
       end
   end.
 
-addNewValue(Station, {{Year, Month, Day}, {Hour, Minute, Second}}, Type, Value, List,
-    [StationNames, StationCoords, Readings]) ->
-  case [{Y, Mon, D, H, Min, S, T, V} || {{{Y, Mon, D}, {H, Min, S}}, T, V} <- List,
+addNewValue(StationKey, {{Year, Month, Day}, Hour}, Type, Value, List, [Stations, Readings]) ->
+  case [{Y, Mon, D, H, T, V} || {{{Y, Mon, D}, H}, T, V} <- List,
     {Y, Mon, D, H, T} == {Year, Month, Day, Hour, Type}] of
-    [] -> [StationNames, StationCoords,
-      dict:append(Station, {{{Year, Month, Day}, {Hour, Minute, Second}}, Type, Value}, Readings)];
+    [] -> [Stations, dict:append(StationKey, {{{Year, Month, Day}, Hour}, Type, Value}, Readings)];
     _ -> io:format("This reading is already registered!~n"), error
   end.
 
 
-removeValue(Station, {{Year, Month, Day}, Hour}, Type, [StationNames, StationCoords, Readings]) ->
+removeValue(Station, {{Year, Month, Day}, Hour}, Type, [Stations, Readings]) ->
   Fun = fun(Val) ->
-    [{{{Y, Mon, D}, {H, Min, S}}, T, V} || {{{Y, Mon, D}, {H, Min, S}}, T, V} <- Val,
+    [{{{Y, Mon, D}, H}, T, V} || {{{Y, Mon, D}, H}, T, V} <- Val,
       {Y, Mon, D, H, T} /= {Year, Month, Day, Hour, Type}]
         end,
-  case {maps:find(Station, StationNames), maps:find(Station, StationCoords)} of
-    {error, error} -> io:format("This station is not registered yet!~n"), error;
-    {{ok, {Name, Coords}}, error} ->
-      removeReading(Name, Coords, Station, Fun, [StationNames, StationCoords, Readings]);
-    {error, {ok, {Name, Coords}}} ->
-      removeReading(Name, Coords, Station, Fun, [StationNames, StationCoords, Readings])
-  end.
-
-removeReading(Name, Coords, Station, Fun, [StationNames, StationCoords, Readings]) ->
-  case dict:is_key(Station, Readings) of
-    true ->
-      [StationNames, StationCoords, dict:update(Station, Fun, Readings)];
-    false ->
-      case dict:is_key(Name, Readings) of
+  case maps:find(Station, Stations) of
+    error -> io:format("This station is not registered yet!~n"), error;
+    {ok, StationKey} ->
+      case dict:is_key(StationKey, Readings) of
         true ->
-          [StationNames, StationCoords, dict:update(Name, Fun, Readings)];
-        false ->
-          case dict:is_key(Coords, Readings) of
-            true ->
-              [StationNames, StationCoords, dict:update(Coords, Fun, Readings)];
-            false -> io:format("The key was not found in dictionary. How could this happen?~n"), superError
-          end
+          [Stations, dict:update(StationKey, Fun, Readings)];
+        false -> io:format("This station does not have any readings yet!~n")
       end
   end.
 
+
+getOneValue(Station, {{Year, Month, Day}, Hour}, Type, [Stations, Readings]) ->
+  case maps:find(Station, Stations) of
+    error -> io:format("This station is not registered yet!~n"), error;
+    {ok, StationKey} ->
+      case dict:is_key(StationKey, Readings) of
+        false -> io:format("This station does not have any readings yet!~n");
+        true ->
+          case [{{{Y, Mon, D}, H}, T, V} || {{{Y, Mon, D}, H}, T, V} <- dict:fetch(StationKey, Readings),
+            {Y, Mon, D, H, T} == {Year, Month, Day, Hour, Type}] of
+            [Result] -> Result;
+            [] -> io:format("There's no such a reading!~n"), error
+          end
+      end
+  end.
 
