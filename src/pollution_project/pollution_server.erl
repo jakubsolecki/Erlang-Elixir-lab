84189420bd78005e3db31d34ce7645ed1b54a690
Jakub <jakub.solecki@onet.com.pl>
2020-04-19 13:52:17 +0200

    added client to server

--- src/pollution_project/pollution_server.erl
+++ src/pollution_project/pollution_server.erl
@@ -22,91 +22,90 @@ init() -> loop(pollution:createMonitor()).
 
 stop() -> pollServ ! stop.
 
+%% Main sever loop
 loop(Storage) ->
   receive
-    {add_station, Name, Coords} ->
+    {PID, {add_station, Name, Coords}} ->
       UpdatedStorage = pollution:addStation(Name, Coords, Storage),
       case is_list(UpdatedStorage) of
-        true -> loop(UpdatedStorage);
-        false -> ret_val(UpdatedStorage), loop(Storage)
+        true ->
+          PID ! {reply, ok},
+          loop(UpdatedStorage);
+        false ->
+          PID ! {reply, UpdatedStorage},
+          loop(Storage)
       end;
-    {add_values, Station, Date, Type, Value} ->
+    {PID, {add_values, Station, Date, Type, Value}} ->
       UpdatedStorage = pollution:addValues(Station, Date, Type, Value, Storage),
       case is_list(UpdatedStorage) of
-        true -> loop(UpdatedStorage);
-        false -> ret_val(UpdatedStorage), loop(Storage)
+        true ->
+          PID ! {reply, ok},
+          loop(UpdatedStorage);
+        false ->
+          PID ! {reply, UpdatedStorage},
+          loop(Storage)
       end;
-    {remove_value, Station ,Date, Type} ->
+    {PID, {remove_value, Station ,Date, Type}} ->
       UpdatedStorage = pollution:removeValue(Station, Date, Type, Storage),
       case is_list(UpdatedStorage) of
-        true -> loop(UpdatedStorage);
-        false -> ret_val(UpdatedStorage), loop(Storage)
+        true ->
+          PID ! {reply, ok},
+          loop(UpdatedStorage);
+        false ->
+          PID ! {reply, UpdatedStorage},
+          loop(Storage)
       end;
-    {get_one_value, Station ,Date, Type} ->
+    {PID, {get_one_value, Station ,Date, Type}} ->
       Val = pollution:getOneValue(Station, Date, Type, Storage),
-      case Val of
-        {error, _} -> ret_val(Val), loop(Storage);
-        _ ->
-          io:format("Value of ~s from station ~w at ~w is ~w~n", [Type, Station, Date, Val]),
-          loop(Storage)
-      end;
-    {get_station_mean, Station, Type} ->
+      PID ! {reply, Val},
+      loop(Storage);
+    {PID, {get_station_mean, Station, Type}} ->
       Mean = pollution:getStationMean(Station, Type, Storage),
-      case Mean of
-        {error, _} -> ret_val(Mean), loop(Storage);
-        _ ->
-          io:format("Mean of ~s from station ~w is ~w~n", [Type, Station, Mean]),
-          loop(Storage)
-      end;
-    {get_daily_mean, Date, Type} ->
+      PID ! {reply, Mean},
+      loop(Storage);
+    {PID, {get_daily_mean, Date, Type}} ->
       Mean = pollution:getDailyMean(Date, Type, Storage),
-      case Mean of
-        {error, _} -> ret_val(Mean), loop(Storage);
-        _ ->
-          io:format("Daily mean of ~s is ~w~n", [Type, Mean]),
-          loop(Storage)
-      end;
-    {get_hourly_mean, Station, Hour, Type} ->
+      PID ! {reply, Mean},
+      loop(Storage);
+    {PID, {get_hourly_mean, Station, Hour, Type}} ->
       Mean = pollution:getHourlyMean(Station, Hour, Type, Storage),
-      case Mean of
-        {error, _} -> ret_val(Mean), loop(Storage);
-        _ ->
-          io:format("Mean of ~s at ~w:00 at ~w is ~w~n", [Type, Hour, Station, Mean]),
-          loop(Storage)
-      end;
-    {get_seasonal_mean, Station, Start, End, Type} ->
+      PID ! {reply, Mean},
+      loop(Storage);
+    {PID, {get_seasonal_mean, Station, Start, End, Type}} ->
       Mean = pollution:getSeasonalMean(Station, Start, End, Type, Storage),
-      case Mean of
-        {error, _} -> ret_val(Mean), loop(Storage);
-        _ ->
-          io:format("Mean of ~s at ~w for given interval of time is ~w~n", [Type, Station, Mean]),
-          loop(Storage)
-      end;
+      PID ! {reply, Mean},
+      loop(Storage);
     stop -> ok
   end.
 
-ret_val(Val) -> io:format("~w~n", [Val]).
+%% Client
+call(Msg) ->
+  pollServ ! {self(), Msg},
+  receive
+    {reply, Reply} -> Reply
+  end.
 
+%% Requesting operations on server via client (call)
 addStation(Name, {Long, Lat}) ->
-  pollServ ! {add_station, Name, {Long, Lat}}.
+  call({add_station, Name, {Long, Lat}}).
 
 addValues(Station, {{Year, Month, Day}, {Hour, Minutes, Seconds}}, Type, Value) ->
-  pollServ ! {add_values, Station, {{Year, Month, Day}, {Hour, Minutes, Seconds}}, Type, Value}.
+  call({add_values, Station, {{Year, Month, Day}, {Hour, Minutes, Seconds}}, Type, Value}).
 
 removeValue(Station, {{Year, Month, Day}, Hour}, Type) ->
-  pollServ ! {remove_value, Station, {{Year, Month, Day}, Hour}, Type}.
+  call({remove_value, Station, {{Year, Month, Day}, Hour}, Type}).
 
 getOneValue(Station, {{Year, Month, Day}, Hour}, Type) ->
-  pollServ ! {get_one_value, Station, {{Year, Month, Day}, Hour}, Type}.
+  call({get_one_value, Station, {{Year, Month, Day}, Hour}, Type}).
 
 getStationMean(Station, Type) ->
-  pollServ ! {get_station_mean, Station ,Type}.
+  call({get_station_mean, Station ,Type}).
 
 getDailyMean({Year, Month, Day}, Type) ->
-  pollServ ! {get_daily_mean, {Year, Month, Day}, Type}.
+  call({get_daily_mean, {Year, Month, Day}, Type}).
 
 getHourlyMean(Station, Hour, Type) ->
-  pollServ ! {get_hourly_mean, Station, Hour, Type}.
+  call({get_hourly_mean, Station, Hour, Type}).
 
 getSeasonalMean(Station, {StartYear, StartMonth}, {EndYear, EndMoth}, Type) ->
-  pollServ ! {get_seasonal_mean, Station, {StartYear, StartMonth}, {EndYear, EndMoth}, Type}.
+  call({get_seasonal_mean, Station, {StartYear, StartMonth}, {EndYear, EndMoth}, Type}).
--- src/pollution_project/pollution_server_tests.erl
+++ src/pollution_project/pollution_server_tests.erl
@@ -8,6 +8,7 @@
 %%%-------------------------------------------------------------------
 -module(pollution_server_tests).
 -author("Jakub Solecki").
+
 -include_lib("eunit/include/eunit.hrl").
 -compile(export_all).
 
@@ -15,3 +16,7 @@
 %% API
 -export([]).
 
+%%first_test() ->
+%%  pollution_server:start(),
+%%  T = calendar:local_time(),
+%%  ?assertMatch({add_values, "D17", T, "temp", 20}, pollution_server:addValues("D17", calendar:local_time(), "temp", 20)).
